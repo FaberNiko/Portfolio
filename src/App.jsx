b7ce1553bdd6ce8a706d76d522ffb48a4eece15a
Nikodem <nikodem.faber@interia.pl>
2024-05-25 11:38:18 +0200

    changed name

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,23 @@
-import { useState } from "react";
 import React from "react";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/pages/Home"
-import About from "./components/pages/About"
-import Tech from "./components/pages/Tech"
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Tech from "./components/pages/Tech";
 import Projects from "./components/pages/Projects";
 import Contact from "./components/pages/Contact";
 
-
-
-
-
 function App() {
 	return (
-    <>
-    <Navbar/>
-    <Home/>
-    <About/>
-    <Tech/>
-    <Projects/>
-    <Contact/>
-
-    </>
-     
-	)
+		<>
+			<Navbar />
+			<Home />
+			<About />
+			<Tech />
+			<Projects />
+			<Contact />
+		</>
+	);
 }
 
 export default App;
--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,25 +1,34 @@
 import React, { useEffect } from "react";
 import "../sass/About.scss";
-import myPhoto from "../img/ja.jpg"
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import { motion } from "framer-motion"
-function About  ()  {
-    useEffect(() => {
-        AOS.init({duration: 1000})
-    }, [])
- 
-    return (
-        <section  className="about-section section" id="about">
-          <div className="line"></div>
-            <h2 className="about-section__title" data-aos="fade-left">About me</h2>
-            <div className="about-section__container" data-aos="fade-right">
-                <img className="photo" src={myPhoto} alt="photo of myself" />
-                <p className="about-text">Hi! I'm Nikodem, a junior fronted developer based in Poland ( Cracow ) currently looking for a full-time job. I am fast learning person and I want to get my first commercial experience and gain as much knowledge as I can. </p>
-                <a href="#tech"><button className="show-tech">tech stack</button></a>
-            </div>
-        </section>
-    )
+import myPhoto from "../img/ja.jpg";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+function About() {
+	useEffect(() => {
+		AOS.init({ duration: 1000 });
+	}, []);
+
+	return (
+		<section className="about-section section" id="about">
+			<div className="line"></div>
+			<h2 className="about-section__title" data-aos="fade-left">
+				About me
+			</h2>
+			<div className="about-section__container" data-aos="fade-right">
+				<img className="photo" src={myPhoto} alt="photo of myself" />
+				<p className="about-text">
+					Hi! I'm Nikodem, a junior fronted developer based in Poland ( Cracow )
+					currently looking for a full-time job. I am fast learning person and I
+					want to get my first commercial experience and gain as much knowledge
+					as I can.{" "}
+				</p>
+				<a href="#tech">
+					<button className="show-tech">tech stack</button>
+				</a>
+			</div>
+		</section>
+	);
 }
 
-export default About
+export default About;
